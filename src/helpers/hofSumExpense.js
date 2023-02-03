f101aeb55e210cd83bd83e200a1245f13fa11c16
Wallace <wallace_031@hotmail.com>
2023-02-03 00:12:04 -0300

    4. Salve todas as informações do formulário no estado global

--- src/helpers/hofSumExpense.js
+++ src/helpers/hofSumExpense.js
@@ -1,6 +1,6 @@
 /**
  * @param {*} array - array de objetos
- * @returns retorna a soma de todos os valores de um array
+ * @returns retorna a soma de todos os valores de um array.
  */
 const hofSumExpense = (array) => array.reduce((acc, curr) => acc + (Number(curr
   .value * curr.exchangeRates[curr.currency].ask)), 0);
